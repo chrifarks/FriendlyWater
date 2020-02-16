991aaa78c61b568e27046525c760e60df54e59b5
iobruno <bruno.ariev@gmail.com>
2020-02-16 19:31:01 -0300

    setting up main app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import conf
+import yaml
+from friendlywater.water_stream import *
+from importlib.resources import open_text
+from typing import TextIO
+from pandas import DataFrame
+
+
+def load_csv(file: TextIO, cols) -> DataFrame:
+    return pd.read_csv(file, delimiter=';', names=cols, header=0)
+
+
+data = yaml.load(open_text(conf, 'application.yaml'), Loader=yaml.FullLoader)
+
+water_stream = WaterStream(
+    slope=data.get('stream').get('slope'),
+    conveyance=data.get('stream').get('conveyance'),
+    delta_t=data.get('stream').get('delta_t'),
+    delta_x=data.get('stream').get('delta_x')
+)
+water_channel = WaterChannel(
+    length_in_meters=data.get('channel').get('length'),
+    height_in_meters=data.get('channel').get('height'),
+    width_in_meters=data.get('channel').get('width')
+)
+
+initial = load_csv(file=open_text(conf, 'initial_conditions.csv'),
+                   cols=['time', 'q0', 'h0'])
+
+bound_left = load_csv(file=open_text(conf, 'boundaries_left.csv'),
+                      cols=['time', 'q_left', 'h_left'])
+
+bound_right = load_csv(file=open_text(conf, 'boundaries_right.csv'),
+                       cols=['time', 'q_right', 'h_right'])
+
+boundaries = pd.DataFrame({
+    'time': bound_left['time'],
+    'q_left': bound_left['q_left'],
+    'q_right': bound_right['q_right'],
+    'h_left': bound_left['h_left'],
+    'h_right': bound_right['h_right']
+})
